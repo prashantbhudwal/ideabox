3cf53cbce3c763cff74f877dfc122b8c819c2dd2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-01-20 13:40:59 +0530

    refactor Navbar component; extract navigation items into a separate NavItems component

--- components/nav.tsx
+++ components/nav.tsx
@@ -17,21 +17,27 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              );
-            })}
-          </div>
+          <h1 className="text-2xl font-bold">Ideabox</h1>
         </nav>
       </div>
     </aside>
   );
 }
+
+const NavItems = function () {
+  return (
+    <div className="flex flex-row space-x-0 pr-10">
+      {Object.entries(navItems).map(([path, { name }]) => {
+        return (
+          <Link
+            key={path}
+            href={path}
+            className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+          >
+            {name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
