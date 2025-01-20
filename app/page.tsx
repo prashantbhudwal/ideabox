7a09141c4395418448e7e732313ed2d5004f1be3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-01-20 16:46:34 +0530

    sort blog posts by date in descending order; simplify post link display

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,18 @@ import { getAllPosts } from "@/lib/posts";
 export default async function BlogPage() {
   const posts = await getAllPosts();
 
+  // Sort posts by date in descending order
+  posts.sort(
+    (a, b) =>
+      new Date(b.metadata.date).getTime() - new Date(a.metadata.date).getTime()
+  );
+
   return (
     <div>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>
-              {post.metadata.title} - {post.metadata.date}
-            </Link>
+            <Link href={`/blog/${post.slug}`}>{post.metadata.title}</Link>
           </li>
         ))}
       </ul>
