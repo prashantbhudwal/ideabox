661f8b2490823b0d8462d8cdbb868613c9a2e769
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-03-16 21:23:13 +0530

    feat: wip sweetener post

--- components/markdown-parser/post-components.tsx
+++ components/markdown-parser/post-components.tsx
@@ -1,5 +1,7 @@
 import { bronnComponents } from "@/content/posts/bronn";
+import { sweetenerOptions } from "@/content/posts/sweetener-options";
 
 export const postComponents = {
   ...bronnComponents,
+  ...sweetenerOptions,
 };
