0107ab8dba0bec96dd9be5170ca2dceab8ac1bb6
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-04-10 16:36:58 +0530

    feat: enhance WIP component to accept custom text and improve padding

--- components/wip.tsx
+++ components/wip.tsx
@@ -1,18 +1,22 @@
 import { cn } from "@/lib/utils";
 import { TbShovel } from "react-icons/tb";
 
-export function WIP({ className }: { className?: string }) {
+export function WIP({
+  className,
+  text = "This website is still a work in progress",
+}: {
+  className?: string;
+  text?: string;
+}) {
   return (
     <div
       className={cn(
-        "text-muted-foreground/60 rounded py-1  flex items-center space-x-2 italic",
+        "text-muted-foreground/60 rounded py-4 flex items-center space-x-2 italic",
         className,
       )}
     >
       <TbShovel />
-      <span className="sm:inline hidden">
-        This website is still a work in progress.
-      </span>
+      <span className="sm:inline hidden">{text}</span>
       <span className="sm:hidden">WIP</span>
     </div>
   );
