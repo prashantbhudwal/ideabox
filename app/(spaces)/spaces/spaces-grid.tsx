4bb67269ea5fb3e5e783fea9e4dcddb685ee34cd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-05-16 22:59:41 +0530

    refactor and restyle

---
+++ app/(spaces)/spaces/spaces-grid.tsx
@@ -0,0 +1,59 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { url } from "../../url";
+import { spaces } from "./spaces";
+
+export function SpacesGrid() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2 auto-rows-[250px]">
+      {spaces.map((space, index) => {
+        return (
+          <Link
+            href={url.spaceById({ id: space.id })}
+            key={space.id}
+            className={cn(
+              "h-full",
+              index === 0 ? "col-span-1 md:col-span-3" : "",
+              index === 1 ? "col-span-1 md:col-span-2" : "",
+            )}
+          >
+            {space.heroImage && (
+              <Card
+                className={cn(
+                  "relative h-full",
+                  "hover:outline-primary hover:outline transition-colors duration-300 p-0.5 backdrop-blur-xs hover:backdrop-blur-none",
+                )}
+              >
+                <div className="relative w-full h-full overflow-hidden">
+                  <Image
+                    className="rounded object-cover"
+                    src={url.images.spaces({ imgName: space.heroImage })}
+                    fill
+                    alt=""
+                  />
+                </div>
+                <div className="absolute left-2 bottom-2 w-full  p-2 rounded">
+                  <CardHeader>
+                    <CardTitle className="text-2xl sm:text-3xl md:text-4xl font-extrabold text-foreground font-serif drop-shadow-md">
+                      {space.name}
+                    </CardTitle>
+                    <CardDescription className="text-sm sm:text-base md:text-lg text-foreground/80 bg-black/10 backdrop-blur-xs">
+                      {space.desc}
+                    </CardDescription>
+                  </CardHeader>
+                </div>
+              </Card>
+            )}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
--- app/(spaces)/spaces/spaces.ts
+++ app/(spaces)/spaces/spaces.ts
@@ -1,12 +1,15 @@
 import { SimilaritySpace } from "./similarity";
 import { SweetenerSpace } from "./sweetener";
 
+type tagOptions = "health" | "ai" | "software" | "thinking";
+
 type SpaceDefinition = {
   id: string;
   name: string;
   desc: string;
   heroImage: string;
   Component: React.ComponentType;
+  tags: tagOptions[];
 };
 
 export const spaces = [
@@ -16,6 +19,7 @@ export const spaces = [
     desc: "Which sweeteners raise GI? What sweeteners are trojan horses? Which ones are actually good?",
     Component: SweetenerSpace,
     heroImage: "sweeteners.webp",
+    tags: ["health", "software"],
   },
   {
     id: "similarity",
@@ -23,6 +27,7 @@ export const spaces = [
     desc: "Find similarity between two texts using OpenAI embeddings.",
     Component: SimilaritySpace,
     heroImage: "similarity.webp",
+    tags: ["ai", "software"],
   },
 ];
 
