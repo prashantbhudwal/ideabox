19fec3528363357a87f3aa5bf24b1fca9a9075ae
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-05-25 21:17:07 +0530

    add function for contextual chunking

---
+++ server/vector/chunk-context.ts
@@ -0,0 +1,44 @@
+import { TPostChunk } from "./chunk";
+import { TPost } from "@/lib/types/post";
+import { mdxToGfmMarkdown } from "./mdx-to-markdown";
+import dedent from "dedent";
+import { llama70B } from "@/lib/models";
+import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export const generateChunkContext = async function ({
+  chunk,
+  post,
+}: {
+  chunk: TPostChunk;
+  post: TPost;
+}) {
+  const postMarkdown = await mdxToGfmMarkdown(post.content);
+
+  const { text: context } = await generateText({
+    model: openai("gpt-4.1-nano"),
+    prompt: dedent`
+<document>
+${postMarkdown}
+</document>
+Here is the chunk we want to situate within the whole document
+<chunk>
+${chunk.text}
+</chunk>
+Please give a short succinct context to situate this chunk within the overall document for the purposes of improving search retrieval of the chunk. Answer only with the succinct context and nothing else.
+`,
+  });
+
+  const contextualChunkText = `${context}\n\n${chunk.text}`;
+
+  const updatedChunkObject = {
+    ...chunk,
+    text: contextualChunkText,
+    metadata: {
+      ...chunk.metadata,
+      context,
+    },
+  };
+
+  return updatedChunkObject;
+};
