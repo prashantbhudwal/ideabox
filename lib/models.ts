cc6d6c1c4d3db0b7f6fd9945bd6489befc12f033
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-05-25 21:22:16 +0530

    add more models

--- lib/models.ts
+++ lib/models.ts
@@ -1,3 +1,15 @@
 import { google } from "@ai-sdk/google";
+import { groq } from "@ai-sdk/groq";
+import { openai } from "@ai-sdk/openai";
 
 export const gemini = google("gemini-2.0-flash");
+export const gpt41Mini = openai("gpt-4.1-mini");
+export const qwen32Bq2q = groq("qwen-qwq-32b");
+export const llama70B = groq("llama-3.3-70b-versatile");
+
+export const models = {
+  gemini,
+  gpt41Mini,
+  qwen32Bq2q,
+  llama70B,
+};
