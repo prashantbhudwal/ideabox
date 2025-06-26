9fadf361c5b29f194836c30b2cf122a1d5c0b174
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2025-06-26 18:18:54 +0530

    feat: enhance chat composer with textarea and updated submit button design

--- src/client/components/chat/blog-agent/chat.composer.tsx
+++ src/client/components/chat/blog-agent/chat.composer.tsx
@@ -3,6 +3,8 @@ import { Input } from "~/client/components/ui/input";
 import { useAgentStore } from "./agent.store";
 import { useRef, useEffect } from "react";
 import { SelectedText } from "./chat.selected-text";
+import { ArrowUp, SendIcon } from "lucide-react";
+import { Textarea } from "../../ui/textarea";
 
 export function Composer({
   handleCustomSubmit,
@@ -11,7 +13,7 @@ export function Composer({
 }: {
   handleCustomSubmit: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
   input: string;
-  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleInputChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }) {
   const selectedText = useAgentStore((s) => s.selectedText);
   const setSelectedText = useAgentStore((s) => s.setSelectedText);
@@ -34,21 +36,25 @@ export function Composer({
     };
   }, [selectedText, setSelectedText]);
   return (
-    <>
+    <div className="flex flex-col gap-2 p-4">
       <SelectedText />
       <form
         ref={formRef}
         onSubmit={handleCustomSubmit}
-        className="flex gap-2 border-t p-4"
+        className="flex flex-col gap-2 p-4 rounded-xl outline bg-secondary "
       >
-        <Input
+        <Textarea
           value={input}
           onChange={handleInputChange}
           placeholder="ask anything"
-          className="flex-1"
+          className="resize-none border-none focus-visible:ring-0 focus-visible:ring-offset-0"
         />
-        <Button type="submit">Send</Button>
+        <div className="flex justify-end">
+          <Button type="submit" size={"icon"} className="rounded-full size-7">
+            <ArrowUp />
+          </Button>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
